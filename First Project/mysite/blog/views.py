377c148abe278987b3b54a35bb37a13c07f9d593
Zahra-Nasiri <z.nasir.8181@gmail.com>
2022-11-03 15:38:24 +0330

    adding code of first project wich is a blog project

---
+++ First Project/mysite/blog/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+# from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+
+# Create your views here.
+
+# CLASS BASED VIEW FOR POST LIST
+
+# class PostListView(ListView):
+#     """
+#     Alternative post list view
+#     """
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    context = {'posts': posts, 'tag': tag}
+    return render(request, 'blog/post/list.html',context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+            status=Post.Status.PUBLISHED,
+            slug=post,
+            publish__year=year,
+            publish__month=month,
+            publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    context = {'post': post, 'comments': comments, 'form': form}
+
+    return render(request, 'blog/post/detail.html', context)
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                      [cd['to']])
+            sent = True
+    
+    else:
+        form = EmailPostForm()
+    context = {'post': post, 'form': form, 'sent': sent}
+    return render(request, 'blog/post/share.html', context)
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    context = {'post': post, 'form': form, 'comment': comment}
+    return render(request, 'blog/post/comment.html',context)
+
+
+
+
