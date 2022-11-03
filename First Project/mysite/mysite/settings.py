f558cc4e2fe2aea7d74b079623fcce4db427edb2
Zahra-Nasiri <z.nasir.8181@gmail.com>
2022-11-03 15:43:27 +0330

    deleting some personal password

--- First Project/mysite/mysite/settings.py
+++ First Project/mysite/mysite/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'z.nasir.8181@gmail.com'
-EMAIL_HOST_PASSWORD = 'zocydjhnmdazchez'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
